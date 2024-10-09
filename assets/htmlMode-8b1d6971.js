9280066d25736bfc7fd24a765c78274f2fdc14bf
liuxy0551 <liuxy0551@qq.com>
2024-10-09 10:31:33 +0000

    Deploying to gh-pages from @ liuxy0551/monaco-sql-languages@ff35c4578e0a22a3d6a989f5df1f0c18befab20b 🚀

--- assets/htmlMode-8b1d6971.js
+++ assets/htmlMode-8b1d6971.js
@@ -1,4 +1,4 @@
-import{m as ze}from"./index-e1fc602c.js";/*!-----------------------------------------------------------------------------
+import{m as ze}from"./index-aabd779e.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.31.0(252e010eb73ddc2fa1a37c1dade7bf35d87106cd)
  * Released under the MIT license
